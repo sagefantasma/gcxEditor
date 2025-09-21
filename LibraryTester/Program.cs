e64899d9ac352d5ada6856873b0cc871fc9a4b7d
Andy Naro <a.naro1995@gmail.com>
2025-09-21 16:02:34 -0400

    committing the small bit of logic i used to populate the dictionary for potential later use

--- LibraryTester/Program.cs
+++ LibraryTester/Program.cs
@@ -4,10 +4,44 @@ using System.Text.Json.Serialization;
 
 namespace LibraryTester
 {
+    
     internal class Program
     {
+        internal class DictionaryEntry
+        {
+            public int StrCode;
+            public string Name;
+
+            public DictionaryEntry(int strCode, string name)
+            {
+                StrCode = strCode;
+                Name = name;
+            }
+        }
+
+        private static List<DictionaryEntry> ParseDictionaryEntries(FileInfo file)
+        {
+            List<string> fileContents = File.ReadAllLines(file.FullName).ToList();
+            List<DictionaryEntry> dictionaryEntries = new List<DictionaryEntry>();
+            foreach (string line in fileContents)
+            {
+                string[] parts = line.Split(' ');
+                DictionaryEntry entry = new DictionaryEntry(int.Parse(parts[0].Split('x')[1], System.Globalization.NumberStyles.HexNumber), parts[1]);
+                dictionaryEntries.Add(entry);
+            }
+
+            return dictionaryEntries;
+        }
+
         static void Main(string[] args)
         {
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:\\Users\\Andy\\repos\\gcx_decompiler\\dictionaries");
+            List<DictionaryEntry> allDictionaryEntries = new List<DictionaryEntry>();
+            foreach(var file in directoryInfo.GetFiles())
+            {
+                allDictionaryEntries.AddRange(ParseDictionaryEntries(file));
+            }
+            File.WriteAllText("masterDictionary.json",JsonConvert.SerializeObject(allDictionaryEntries));
             /*foreach (string file in Directory.GetFiles("C:\\Users\\yonan\\Documents\\Pinned Folders\\C Drive Steam Games\\MGS2\\assets\\gcx\\eu\\_bp"))
             {
                 //issue in scenerio.gcx
